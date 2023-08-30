6eed351b5241e570ee07e4bc5392f9fd2fbf952f
MDM <mdm.pro4@gmail.com>
2023-08-30 19:07:56 -0300

    Adicionando textos

--- src/components/conhecimentos/Conhecimetos.jsx
+++ src/components/conhecimentos/Conhecimetos.jsx
@@ -7,30 +7,36 @@ const Conhecimentos = () => {
             <p>Conhecimentos</p>
             <ul>
                 <li className="subTitle">Java Script</li>
-                <br/>
+                <br />
                 <p className="text">
-                    É uma linguagem de programação, usada na criação de paginas web. Tem como finalidade, 
-                    adicionar funcionalidades dinâmicas e inkteratividade na pagina, sendo possível criar validações de formulários, 
-                    atualizações de conteúdos, recarregar paginas, animações, etc.. 
-                    </p>
-                <br/>
+                    É uma linguagem de programação, usada na criação de paginas web. Tem como finalidade,
+                    adicionar funcionalidades dinâmicas e inkteratividade na pagina, sendo possível criar validações de formulários,
+                    atualizações de conteúdos, recarregar paginas, animações, etc..
+                </p>
+                <br />
                 <li className="subTitle">React</li>
-                <br/>
+                <br />
                 <p className="text">
                     React é um framework Java Script que veio ao mercado no intuito de facilita a criação de componentes visuais, e atualizações eficientes,
-                     tendo como resultado uma interface mais rapidas e responsivas. 
-                    </p>
-                <br/>
+                    tendo como resultado uma interface mais rapidas e responsivas.
+                </p>
+                <br />
                 <li className="subTitle">Node.js</li>
-                <br/>
+                <br />
                 <p className="text">
                     é considerado um ambiente para a execução de Java script que por sua vez, permite rodar o código.
                     Ele é usado para a criação de aplicações de servidor, tanto para sites como para serviços web. E, tem por finalidade,
                     a facilidade na hora de contruir aplicações de alto desempenho e escaláveis, permitindo tanto os desenvolvedores quanto os clientes,
                     usarem a mesma linguagem.
-                    </p>
-                <br/>
+                </p>
+                <br />
                 <li className="subTitle">Css</li>
+                <br />
+                <p className="text">
+                    É utilizado na estilização de páginas web, tanto na parte de fontes, layout, quanto na parte de controle de cores. É muito comum voce ver ele
+                    sendo usado em conjunto com o Html para criar uma estrutura visual atraente e consistente.
+                </p>
+                <br />
                 <li className="subTitle">Html</li>
                 <li className="subTitle">NoSQL</li>
                 <li className="subTitle">Git GitHub</li>
