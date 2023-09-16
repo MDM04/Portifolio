6a86623e06fa4d042e2d242bc7f6a5f664007fb6
MDM <mdm.pro4@gmail.com>
2023-09-16 16:32:53 -0300

    ok

--- src/components/conhecimentos/Conhecimetos.jsx
+++ src/components/conhecimentos/Conhecimetos.jsx
@@ -1,6 +1,6 @@
 import SubTitle from "../subTitle/SubTitle"
 import "../text/Text.css"
-import "../conhecimentos/conhecimentos.css"
+
 
 const Conhecimentos = () => {
 
@@ -8,81 +8,81 @@ const Conhecimentos = () => {
 
     return (
         <>
-            <p id="Conhecimentos">Conhecimentos</p>
-            <br/>
+            <p>Conhecimentos</p>
+            <br />
             <ul>
-            <SubTitle name="Java Script"/>
+                <SubTitle name="Java Script" />
                 <br />
-                <div className="container-Conhecimentos">
-                <p >
-                    É uma linguagem de programação, usada na criação de paginas web. Tem como finalidade,
-                    adicionar funcionalidades dinâmicas e inkteratividade na pagina, sendo possível criar validações de formulários,
-                    atualizações de conteúdos, recarregar paginas, animações, etc..
-                </p>
+                <div>
+                    <p >
+                        É uma linguagem de programação, usada na criação de paginas web. Tem como finalidade,
+                        adicionar funcionalidades dinâmicas e inkteratividade na pagina, sendo possível criar validações de formulários,
+                        atualizações de conteúdos, recarregar paginas, animações, etc..
+                    </p>
                 </div>
                 <br />
-                <SubTitle name="React"/>
+                <SubTitle name="React" />
                 <br />
-                <div className="container-Conhecimentos">
-                <p >
-                    React é um framework Java Script que veio ao mercado no intuito de facilita a criação de componentes visuais, e atualizações eficientes,
-                    tendo como resultado uma interface mais rapidas e responsivas.
-                </p>
+                <div >
+                    <p >
+                        React é um framework Java Script que veio ao mercado no intuito de facilita a criação de componentes visuais, e atualizações eficientes,
+                        tendo como resultado uma interface mais rapidas e responsivas.
+                    </p>
                 </div>
                 <br />
-                <SubTitle name="Node.js"/>
+                <SubTitle name="Node.js" />
                 <br />
-                <div className="container-Conhecimentos">
-                <p >
-                    é considerado um ambiente para a execução de Java script que por sua vez, permite rodar o código.
-                    Ele é usado para a criação de aplicações de servidor, tanto para sites como para serviços web. E, tem por finalidade,
-                    a facilidade na hora de contruir aplicações de alto desempenho e escaláveis, permitindo tanto os desenvolvedores quanto os clientes,
-                    usarem a mesma linguagem.
-                </p>
+                <div>
+                    <p >
+                        é considerado um ambiente para a execução de Java script que por sua vez, permite rodar o código.
+                        Ele é usado para a criação de aplicações de servidor, tanto para sites como para serviços web. E, tem por finalidade,
+                        a facilidade na hora de contruir aplicações de alto desempenho e escaláveis, permitindo tanto os desenvolvedores quanto os clientes,
+                        usarem a mesma linguagem.
+                    </p>
                 </div>
                 <br />
-                <SubTitle name="Css"/>
+                <SubTitle name="Css" />
                 <br />
-                <div className="container-Conhecimentos">
-                <p >
-                    é considerado um ambiente para a execução de Java script que por sua vez, permite rodar o código.
-                    Ele é usado para a criação de aplicações de servidor, tanto para sites como para serviços web. E, tem por finalidade,
-                    a facilidade na hora de contruir aplicações de alto desempenho e escaláveis, permitindo tanto os desenvolvedores quanto os clientes,
-                    usarem a mesma linguagem.
-                </p>
+                <div>
+                    <p >
+                        é considerado um ambiente para a execução de Java script que por sua vez, permite rodar o código.
+                        Ele é usado para a criação de aplicações de servidor, tanto para sites como para serviços web. E, tem por finalidade,
+                        a facilidade na hora de contruir aplicações de alto desempenho e escaláveis, permitindo tanto os desenvolvedores quanto os clientes,
+                        usarem a mesma linguagem.
+                    </p>
                 </div>
                 <br />
-                <SubTitle name="Html"/>
+                <SubTitle name="Html" />
                 <br />
-                <div className="container-Conhecimentos">
-                <p >
-                    é considerado um ambiente para a execução de Java script que por sua vez, permite rodar o código.
-                    Ele é usado para a criação de aplicações de servidor, tanto para sites como para serviços web. E, tem por finalidade,
-                    a facilidade na hora de contruir aplicações de alto desempenho e escaláveis, permitindo tanto os desenvolvedores quanto os clientes,
-                    usarem a mesma linguagem.
-                </p>
+                <div >
+                    <p >
+                        é considerado um ambiente para a execução de Java script que por sua vez, permite rodar o código.
+                        Ele é usado para a criação de aplicações de servidor, tanto para sites como para serviços web. E, tem por finalidade,
+                        a facilidade na hora de contruir aplicações de alto desempenho e escaláveis, permitindo tanto os desenvolvedores quanto os clientes,
+                        usarem a mesma linguagem.
+                    </p>
                 </div>
                 <br />
-                <SubTitle name="NoSQL"/>
+                <SubTitle name="NoSQL" />
                 <br />
-                <div className="container-Conhecimentos">
-                <p >
-                    é considerado um ambiente para a execução de Java script que por sua vez, permite rodar o código.
-                    Ele é usado para a criação de aplicações de servidor, tanto para sites como para serviços web. E, tem por finalidade,
-                    a facilidade na hora de contruir aplicações de alto desempenho e escaláveis, permitindo tanto os desenvolvedores quanto os clientes,
-                    usarem a mesma linguagem.
-                </p>
+                <div >
+                    <p >
+                        é considerado um ambiente para a execução de Java script que por sua vez, permite rodar o código.
+                        Ele é usado para a criação de aplicações de servidor, tanto para sites como para serviços web. E, tem por finalidade,
+                        a facilidade na hora de contruir aplicações de alto desempenho e escaláveis, permitindo tanto os desenvolvedores quanto os clientes,
+                        usarem a mesma linguagem.
+                    </p>
                 </div>
                 <br />
-                <SubTitle name="GitHub"/>
+                <SubTitle name="GitHub" />
                 <br />
-                <div className="container-Conhecimentos">
-                <p >
-                    é considerado um ambiente para a execução de Java script que por sua vez, permite rodar o código.
-                    Ele é usado para a criação de aplicações de servidor, tanto para sites como para serviços web. E, tem por finalidade,
-                    a facilidade na hora de contruir aplicações de alto desempenho e escaláveis, permitindo tanto os desenvolvedores quanto os clientes,
-                    usarem a mesma linguagem.
-                </p>
+                <div>
+                    <p >
+                        é considerado um ambiente para a execução de Java script que por sua vez, permite rodar o código.
+                        Ele é usado para a criação de aplicações de servidor, tanto para sites como para serviços web. E, tem por finalidade,
+                        a facilidade na hora de contruir aplicações de alto desempenho e escaláveis, permitindo tanto os desenvolvedores quanto os clientes,
+                        usarem a mesma linguagem.
+                    </p>
                 </div>
                 <br />
             </ul>
