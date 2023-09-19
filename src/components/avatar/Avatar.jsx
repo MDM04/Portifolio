175b9091a1f5069a5fcdbb84850eb8f00a5e3c1f
MDM <mdm.pro4@gmail.com>
2023-09-19 20:27:33 -0300

    ok

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -7,7 +7,10 @@ const TelaInicial = () => {
             <div className="containerAvatar">
                 <div className="item-avatar-text" >
                     <p className="item-avatar-paragraph">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, error deserunt! Iure iste provident dolorem soluta cupiditate!
+                        "Seja muito bem-vindo ao meu portfólio! É um prazer tê-lo aqui para explorar o meu trabalho e conhecer um pouco mais sobre o que faço. Espero que encontre inspiração e insight ao navegar por este espaço dedicado às minhas criações. Sinta-se à vontade para explorar cada projeto e, se houver qualquer dúvida ou feedback que queira compartilhar, não hesite em entrar em contato. Agradeço sinceramente por dedicar o seu tempo e interesse ao meu trabalho.
+                         Espero que esta visita seja enriquecedora e que possamos, quem sabe, colaborar em projetos futuros. Aproveite a sua estadia aqui!"
+
+
                     </p>
                 </div>
                 <div className="item-avatar-img"  >
--- src/components/biografia/Biografia.jsx
+++ src/components/biografia/Biografia.jsx
@@ -8,9 +8,19 @@ const Biografia = () => {
         <>
             <SubTitle name="Biografia" />
             <p className="biografia-paragraph">
-                Olá, meu nome é Marcio Muniz Barbosa
-                nascido em 03/10/1995
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptatem adipisci molestiae repellendus? Blanditiis
+            Olá sou o Marcio Muniz Barbosa, um programador experiente com um amplo conhecimento em várias tecnologias de desenvolvimento. Minha jornada no mundo da programação começou com a descoberta do fascinante universo do JavaScript e suas bibliotecas, e desde então venho contribuindo para projetos desafiadores em diversas frentes.
+
+Minha expertise abrange o desenvolvimento front-end, onde utilizo HTML, CSS e JavaScript para criar interfaces intuitivas e visualmente atraentes. Sou proficiente em frameworks como React, o que me permite criar experiências interativas e responsivas de alta qualidade.
+
+Além disso, sou versado em back-end, utilizando Node.js para construir aplicações escaláveis e eficientes. Tenho experiência sólida com bases de dados NoSQL, especialmente MongoDB, e a integração de sistemas de gerenciamento de banco de dados é uma das minhas especialidades.
+
+Trabalhar com Git e GitHub faz parte do meu fluxo de trabalho diário, garantindo um versionamento preciso e uma colaboração eficaz em equipe. Acredito firmemente na importância da integração contínua e na entrega contínua, o que facilita a implementação eficaz de novos recursos e a manutenção constante de alta qualidade.
+
+Durante minha carreira, tive o privilégio de participar de uma variedade de projetos, desde aplicações web dinâmicas até sistemas complexos de gerenciamento de dados. Essas experiências me proporcionaram uma compreensão aprofundada das necessidades dos clientes e a capacidade de entregar soluções que excedem as expectativas.
+
+Estou sempre em busca de aprimoramento e aprendizado contínuo, mantendo-me atualizado com as últimas tendências e inovações no mundo da programação. Acredito que a chave para o sucesso está na combinação de habilidades técnicas sólidas com uma mentalidade criativa e orientada para soluções.
+
+Estou entusiasmado para enfrentar novos desafios e colaborar em projetos inovadores. Se você está em busca de um programador dedicado, apaixonado e orientado para resultados, estou aqui para ajudar a transformar suas ideias em realidade. Não hesite em entrar em contato para discutir como podemos trabalhar juntos."
             </p>
         </>
     )
