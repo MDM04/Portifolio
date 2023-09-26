3cc971f2ea48f88ea3d39fa0dfbf2ef6f2dce8c6
MDM <mdm.pro4@gmail.com>
2023-09-26 19:41:35 -0300

    ok

--- src/components/conhecimentos/Conhecimetos.jsx
+++ src/components/conhecimentos/Conhecimetos.jsx
@@ -1,30 +1,49 @@
 import SubTitle from "../subTitle/SubTitle"
 import "../text/Text.css"
-
+import'../conhecimentos/conhecimentos.css'
 
 const Conhecimentos = () =>{
 
 return (
     <>
-        <br />
+    <div className="container-SubTitle">
+    <br />
 
-            <SubTitle name="Conhecimentos" />
-        <br />
-        <br />
+<SubTitle name="Conhecimentos" />
+<br />
+<br />
+    </div>
+        <div className="container-conhecimentos">
+
+            <div className="container-Js">
             <SubTitle name="Java Script" />
             <br />
+            </div>
+            <div className="container-Rt">
             <SubTitle name="React" />
             <br />
+            </div>
+            <div className="container-Nj">
             <SubTitle name="Node.js" />
             <br />
+            </div>
+            <div className="container-Css">
             <SubTitle name="Css" />
             <br />
+            </div>
+            <div className="container-Ht">
             <SubTitle name="Html" />
             <br />
+            </div>
+            <div className="container-Nq">
             <SubTitle name="NoSQL" />
             <br />
+            </div>
+            <div className="container-Gh">
             <SubTitle name="GitHub" />
             <br />
+            </div>
+        </div>
     </>
 )
 }
